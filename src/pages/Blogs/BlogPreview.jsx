12e370a8dd556ea92415e0132e29e1c98e3097bc
Krishnapal Tomar <krishnapal.01tomar@gmail.com>
2022-08-25 15:01:05 +0530

    introduce useNavigate hook

--- src/pages/Blogs/BlogPreview.jsx
+++ src/pages/Blogs/BlogPreview.jsx
@@ -1,17 +1,19 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './BlogPreview.Styles.css';
 
 const categories = ["Entertainment", "Education", "Computers", "Science"]
 
 const BlogPreview = () => {
+    const navigate = useNavigate();
+
     return (
         <div>
             <h1>Blogs</h1>
             {
                 categories.map((element) => {
                     return (
-                        <div className="link-container">
-                            <Link to={element}> {element} </Link>
+                        <div className="link-container" onClick={() => navigate(element)}>
+                            <span className="link-span"> {element} </span>
                         </div>
                     );
                 })
